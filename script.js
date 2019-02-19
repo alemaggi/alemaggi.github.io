c9be56e1828217966552fdd684df6eac7da32f8e
Alessandro <alessandromaggi1997@libero.it>
2019-02-19 11:53:48 +0100

    typed.js library and custom script for type effect

---
+++ script.js
@@ -0,0 +1,11 @@
+var typed = new Typed(".typed", {
+    strings: ["Computer engineering student", "Maker", "Tech enthusiast"],
+    smartBackspace: true, // Default value
+    typeSpeed: 40,
+    backSpeed: 40,
+    loop: true,
+    showCursor: true,
+    cursorChar: '|',
+    autoInsertCss: true,
+    smartBackspace: true,
+});
